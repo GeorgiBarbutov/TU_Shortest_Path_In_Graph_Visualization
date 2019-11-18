713d68ee0bf27d36894f339100b52e9747d29ce5
GeorgiBarbutov <slepoto@gmail.com>
2019-11-18 20:23:03 +0200

    Initial Commit

---
+++ TU_Shortest_Path_In_Graph_Visualization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TU_Shortest_Path_In_Graph_Visualization
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
